48f97685b58c90b0ec7a286abcc177978fab3cca
David Bilge <david.bilge@gmail.com>
2021-09-25 17:18:27 +0200

    Initial commit

---
+++ src/main/java/de/davidbilge/kata/vvm/VvmApplication.java
@@ -0,0 +1,13 @@
+package de.davidbilge.kata.vvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VvmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/davidbilge/kata/vvm/VvmApplicationTests.java
@@ -0,0 +1,13 @@
+package de.davidbilge.kata.vvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
